a726b79a9ebb9d581a4427583dd2345fa9e039d5
ASJ THILAK <78794112+ASJTHILAK@users.noreply.github.com>
2021-04-29 23:43:25 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value");
+        int num = sc.nextInt();
+        int a=0,b=1,c;
+        System.out.print(a+" "+b);
+        for(int i = 0;i<=num;i++){
+            c=a+b;
+            System.out.print(" "+c);
+            a=b;
+            b=c;            
+        }
+    }    
+}
